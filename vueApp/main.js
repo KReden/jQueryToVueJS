d529000ddb655ea1e5f51c47be41564815b3fdee
Kyle Reden <kylereden@gmail.com>
2017-10-22 18:46:46 -0500

    Move variables into data props

--- vueApp/main.js
+++ vueApp/main.js
@@ -2,6 +2,15 @@
 new Vue({
   el: "#app",
   data: {
-    title: "A Vue.js POS!"
+    title: "A Vue.js POS!",
+    cart: [],
+    salesTax: 0.1,
+    currentId: 1,
+    subtotal: 0,
+    presetItems: [
+      { name: "Chicken", price: 1.99 },
+      { name: "Rice", price: 2.15 },
+      { name: "Potatoes", price: .99 }
+    ],
   },
 })
