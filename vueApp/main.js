2f6b58ddbee4565c64f0f12aedc54f4dceb1cf10
Kyle Reden <kylereden@gmail.com>
2017-10-22 21:59:41 -0500

    Turned Capitalize into a filter

--- vueApp/main.js
+++ vueApp/main.js
@@ -16,6 +16,14 @@ new Vue({
     ]
   },
 
+  filters: {
+    capitalize: (value) => {
+      if(!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    },
+  },
+
   computed: {
     newItem: {
       get(){
