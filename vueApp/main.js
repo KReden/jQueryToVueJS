42060f8c6e5d3435d466eaaeeb0667c7716d5b95
Kyle Reden <kylereden@gmail.com>
2017-10-22 21:53:14 -0500

    Hook up text box and cart list
    Need to turn capotalize into a filter!

--- vueApp/main.js
+++ vueApp/main.js
@@ -7,6 +7,8 @@ new Vue({
     salesTax: 0.1,
     currentId: 1,
     subtotal: 0,
+    itemName: "",
+    itemPrice: null,
     presetItems: [
       { name: "Chicken", price: 1.99, text: "Chicken - 1.99 a lb" },
       { name: "Rice", price: 2.15, text: "Rice - 2.15 a lb" },
@@ -14,11 +16,29 @@ new Vue({
     ]
   },
 
+  computed: {
+    newItem: {
+      get(){
+        return { name: this.itemName, price: this.itemPrice }
+      },
+      set(val){
+        this.itemName = val
+        this.itemPrice = val
+      }
+    },
+    cartEmpty(){
+      return this.cart.length === 0
+    }
+  },
+
   methods:{
     addItemToCart(item){
+      // Don't do anything if no item or item.name/item.price is blank
+      if(!item || item.name === "" || item.price === "") { return }
       // Can pass item object
       // No need to do any DOM id lookup or switch statement
       this.cart.push(item)
+      this.newItem = ""
     }
   }
 })
